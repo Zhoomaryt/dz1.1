248645e4958037c337c4e7a0c0333389c5142635
Пользователь <zoomartdubasev5@gmail.com>
2021-10-29 16:41:22 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String s1 = " У меня есть ";
+        String s2 = " ручки ";
+        String s3 = " но я тебе не дам ";
+        int num = 3;
+        String s = s1 + num + s2 + s3;
+
+        System.out.println(s);
+
+    }
+
+}
